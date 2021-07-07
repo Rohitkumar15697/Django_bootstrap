9e9a5dbf1a55e538ce3b0a7ff9ea43d2eb2ad86b
Rohit Kumar <rk221377@gmail.com>
2021-07-07 18:49:57 +0530

    change all

--- Procfile
+++
@@ -1,1 +0,0 @@
-web:gunicorn firstBootstrap.wsgi --log-file -
--- app1/views.py
+++ app1/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from .forms import myform
-
 
 # Create your views here.
 
+
 def index(request):
-    fm=myform()
-    return render(request,'index.html',{'fm':fm})
+    return render(request, 'index.html')
