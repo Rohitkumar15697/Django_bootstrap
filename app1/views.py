7b46a0c1126d5085e47d499a99162eabad015053
Rohit Kumar <rk221377@gmail.com>
2021-07-08 17:47:45 +0530

    adding db

--- app1/views.py
+++ app1/views.py
@@ -1,7 +1,12 @@
+from app1.forms import ShowDataForm
 from django.shortcuts import render
+from .models import ShowData
+from .forms import ShowDataForm
+
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    form=ShowDataForm()
+    return render(request, 'index.html',{'fm':form})
