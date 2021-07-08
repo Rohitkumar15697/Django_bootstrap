e7233d0f0a7f2d98d7d3bc601e6923adc583ba92
Rohit Kumar <rk221377@gmail.com>
2021-07-08 18:04:30 +0530

    commit

--- app1/forms.py
+++ app1/forms.py
@@ -1,7 +1,1 @@
 from django import forms
-#from .models import ShowData
-'''
-class ShowDataForm(forms.ModelForm):
-    class Meta:
-        #model=ShowData
-        fields=['name','age']'''
--- app1/views.py
+++ app1/views.py
@@ -1,8 +1,5 @@
 from app1.forms import ShowDataForm
 from django.shortcuts import render
-#from .models import ShowData
-#from .forms import ShowDataForm
-
 
 # Create your views here.
 
