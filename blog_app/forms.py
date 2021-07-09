fe39adba02201dd250ccf4059ecd08abd188e445
Rohit Kumar <rk221377@gmail.com>
2021-07-09 11:26:07 +0530

    commit with bootstrap form

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -5,4 +5,4 @@ class ShowDataForm(forms.ModelForm):
     class Meta:
         model=ShowData
         fields="__all__"
-        widgets = {'post': forms.Textarea(attrs={'style': 'height: 200px;width:400px'})}
+        widgets = {'name':forms.TextInput(attrs={'class':'form-control'}),'post': forms.Textarea(attrs={'class':'form-control','style':'height:200px;'})}
