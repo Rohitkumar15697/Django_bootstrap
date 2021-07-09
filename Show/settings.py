eaa78ecb059fd636b207aa939ce43db17e6cd52b
Rohit Kumar <rk221377@gmail.com>
2021-07-09 16:34:18 +0530

    postgres database

--- Show/settings.py
+++ Show/settings.py
@@ -82,11 +82,22 @@ WSGI_APPLICATION = 'Show.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgres',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}'''
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'coolcoder_db',
+		'USER':'postgres',
+		'PASSWORD':'root',
+		'HOST':'localhost',
+		'PORT':'5432'
     }
 }
 
